4ed6c17e760c42ba42dfafe2a6cb3ccc5b26ac9d
Kiltae <loverpleasestayo0@gmail.com>
2024-05-12 20:03:23 +0900

    [FEAT] 맴버 DTO 매핑

---
+++ src/main/java/com/example/shopping/controller/req/MemberSignupRequest.java
@@ -0,0 +1,41 @@
+package com.example.shopping.controller.req;
+
+import com.example.shopping.domain.RoleType;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class MemberSignupRequest {
+    @NotNull(message = "로그인 아이디를 입력하세요.")
+    private String loginId;         // 로그인아이디
+
+    @NotNull(message = "비밀번호를 입력하세요.")
+    private String password;        //비밀번호
+
+    @NotNull(message = "이름을 입력하세요.")
+    private String name;            //이름
+
+    @NotNull(message = "우편번호를 입력하세요.")
+    private String zipcode;         //우편번호
+
+    @NotNull(message = "상세주소를 입력하세요.")
+    private String detailAddress;   //상세주소
+
+    @Email(message = "이메일 형식으로 입력하세요")
+    private String email;           //이메일
+
+    @NotNull(message = "전화번호를 입력하세요.")
+    private String phone;           //핸드폰번호
+
+    @NotNull(message = "권한을 입력하세요.")
+    private List<RoleType> roles;    // 권한타입
+}
---
+++ src/main/java/com/example/shopping/controller/res/MemberResponse.java
@@ -0,0 +1,32 @@
+package com.example.shopping.controller.res;
+import java.util.List;
+
+import lombok.*;
+import com.example.shopping.domain.Member;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class MemberResponse {
+
+    private String loginId;         //회원 ID
+    private String name;            //이름
+    private String zipcode;         //우편번호
+    private String detailAddress;   //상세주소
+    private String email;           //이메일
+    private String phone;           //핸드폰번호
+    private List<String> roles;
+
+    // Member -> MemberResponse 변환
+    public static MemberResponse toResponse(Member m) {
+        return MemberResponse.builder()
+                .loginId(m.getLoginId())
+                .name(m.getName())
+                .zipcode(m.getZipcode())
+                .detailAddress(m.getDetailAddress())
+                .email(m.getEmail())
+                .phone(m.getPhone())
+                .build();
+    }
+}
