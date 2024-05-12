c2adc96855427ea5dba1ef703e1253bf6035f567
Kiltae <loverpleasestayo0@gmail.com>
2024-05-12 20:02:04 +0900

    [FEAT] 권한 repository 계층 개발

---
+++ src/main/java/com/example/shopping/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.example.shopping.repository;
+
+import com.example.shopping.domain.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
