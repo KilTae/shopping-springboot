fd55685cf68b454164a62d0df14e549f91d7a181
Kiltae <loverpleasestayo0@gmail.com>
2024-05-12 20:05:35 +0900

    [FEAT] 상품 service 계층 세팅

--- src/main/java/com/example/shopping/service/Impl/ProductServiceImpl.java
+++ src/main/java/com/example/shopping/service/Impl/ProductServiceImpl.java
@@ -11,7 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class ProductServiceImpl implements ProductService {
 
     // 상품 등록 - 이미지 일단은 미포함으로 구현
-    
+
 
     // 상품 조회
 
