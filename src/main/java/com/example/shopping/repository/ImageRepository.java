a55935ec3df00d4170bdcd8b785863baa0c47a89
Kiltae <loverpleasestayo0@gmail.com>
2024-05-18 16:36:11 +0900

    [FEAT] image repository 계층 개발

---
+++ src/main/java/com/example/shopping/repository/ImageRepository.java
@@ -0,0 +1,7 @@
+package com.example.shopping.repository;
+import com.example.shopping.domain.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+public interface ImageRepository extends JpaRepository<Image, Long> {
+}
