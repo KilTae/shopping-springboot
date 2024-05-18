357ce8753fc8b843083122f497b5a1eeadae923c
Kiltae <loverpleasestayo0@gmail.com>
2024-05-18 20:07:26 +0900

    [FEAT] 상품 조회 service 계층 개발

--- src/main/java/com/example/shopping/service/ProductService.java
+++ src/main/java/com/example/shopping/service/ProductService.java
@@ -1,6 +1,9 @@
 package com.example.shopping.service;
 
 import com.example.shopping.controller.req.ProductCreateRequest;
+import com.example.shopping.controller.res.ProductResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -9,6 +12,10 @@ public interface ProductService{
 
     //List<MultipartFile> imgPaths
     void productCreate(ProductCreateRequest productCreateRequest) throws IOException;
+
+   // Page<ProductResponse> productFindAll(Pageable pageable);
+
+    ProductResponse productDetailFind(Long productId);
 
 
 }
