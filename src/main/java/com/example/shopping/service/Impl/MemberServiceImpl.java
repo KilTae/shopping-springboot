ec86eef89b7b86008d1d145907ed77711396614b
Kiltae <loverpleasestayo0@gmail.com>
2024-05-18 11:30:05 +0900

    [FEAT] 내 정보 조회 service 생성

--- src/main/java/com/example/shopping/service/Impl/MemberServiceImpl.java
+++ src/main/java/com/example/shopping/service/Impl/MemberServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.shopping.service.Impl;
 
 import com.example.shopping.controller.req.LoginRequest;
 import com.example.shopping.controller.req.MemberSignupRequest;
+import com.example.shopping.controller.res.MemberResponse;
 import com.example.shopping.domain.LoginType;
 import com.example.shopping.domain.Role;
 import com.example.shopping.domain.RoleType;
@@ -18,11 +19,18 @@ import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.beans.factory.annotation.Value;
+
+import java.util.Collection;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -95,6 +103,24 @@ public class MemberServiceImpl implements MemberService {
     private void DuplicatedLoginIdCheck(boolean duplicatedCheck) {
         if(duplicatedCheck) throw new BusinessException(ErrorCode.DUPLICATED_LOGIN_ID);
 
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public MemberResponse findByDetailMyInfo() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        System.out.println("인증:");
+        System.out.println(authentication.getName());
+        Member member = memberRepository.findByLoginId(authentication.getName()).orElseThrow(() -> new BusinessException(ErrorCode.NOT_FOUND_MEMBER));
+
+        MemberResponse memberResponse = MemberResponse.toResponse(member);
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+
+        List<String> list = authorities.stream().map(GrantedAuthority::getAuthority).toList();
+        memberResponse.setRoles(list);
+
+        return memberResponse;
     }
 
 }
