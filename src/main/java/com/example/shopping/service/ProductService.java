e6c8bc990ae79d47d2a3b652fb1ddcbaeaba1bad
Kiltae <loverpleasestayo0@gmail.com>
2024-06-15 12:48:03 +0900

    [FEAT] 상품 수정 service 계층

--- src/main/java/com/example/shopping/service/ProductService.java
+++ src/main/java/com/example/shopping/service/ProductService.java
@@ -1,7 +1,10 @@
 package com.example.shopping.service;
 
 import com.example.shopping.controller.req.ProductCreateRequest;
+import com.example.shopping.controller.req.ProductEditRequest;
+import com.example.shopping.controller.req.UpdateProductRequest;
 import com.example.shopping.controller.res.ProductResponse;
+import com.example.shopping.controller.res.UpdateProductResponse;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.multipart.MultipartFile;
@@ -13,7 +16,15 @@ public interface ProductService{
 
     void productCreate(ProductCreateRequest productCreateRequest, List<MultipartFile> imgPaths) throws IOException;
 
-   // Page<ProductResponse> productFindAll(Pageable pageable);
+    Page<ProductResponse> productFindAll(Pageable pageable);
+
+    Page<ProductResponse> productFindKeyword(String keyword, Pageable pageable);
+
+    void productDelete(Long productId);
+
+    List<UpdateProductResponse> checkProductUpdate(List<UpdateProductRequest> updateCheckRequest);
+
+    void productEdit(Long goodsId, ProductEditRequest goodsEditRequest, List<MultipartFile> imgPaths);
 
     ProductResponse productDetailFind(Long productId);
 
