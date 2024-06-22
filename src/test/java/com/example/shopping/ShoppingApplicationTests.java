e864ac0a95863be04701f826b87bc5e73bec9e9f
Kiltae <loverpleasestayo0@gmail.com>
2024-06-22 14:05:00 +0900

    [FIX] yml qureydsl 세팅 변경

--- src/test/java/com/example/shopping/ShoppingApplicationTests.java
+++ src/test/java/com/example/shopping/ShoppingApplicationTests.java
@@ -3,10 +3,10 @@ package com.example.shopping;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class ShoppingApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 
